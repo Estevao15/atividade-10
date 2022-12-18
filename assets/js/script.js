ab627798a91feab59e67d87f4d4fbff3d2681e8d
Estevão Henrique <113246534+Estevao15@users.noreply.github.com>
2022-12-18 00:19:33 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const antiga_header = document.querySelector('.header.main')
+antiga_header.insertAdjacentHTML('afterbegin','<h1 class="h1_class">Criando um h1 com insertAdjacentHTML</h1>')
+
+const main = document.querySelector('.header.main')
